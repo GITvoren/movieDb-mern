c5a47cfb7200a422a6a745d956230bee494e6847
GITvoren <tejuco.voren@gmail.com>
2023-07-09 16:25:04 +0800

    refactored dropdown code

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,6 +1,6 @@
 import navbar from '../assets/partial-css/navbar.css';
 import { Link } from 'react-router-dom';
-import { useState, useRef, useEffect } from 'react';
+import { useState, useRef } from 'react';
 
 
 
@@ -11,17 +11,6 @@ function Navbar(){
      const [isFocused, setIsFocused] = useState(false);
      const [query, setQuery] = useState("");
      const ref = useRef(null);
-     const [isHovered, setIsHovered] = useState(false);
-     const [isToggled, setIsToggled] = useState(false);
-
-     const handleToggle = () => {
-          if(!isToggled){
-               setIsToggled(true)
-          } else{
-               setIsToggled(false)
-          }
-     }
-
 
 
      return(
@@ -35,43 +24,15 @@ function Navbar(){
                               <Link>MOVIES</Link>
                               <Link>TV SHOWS</Link>
                               <Link>UPCOMING</Link>
-                              <Link>
-                                   <div
-                                   className="dropdown"
-                                   onMouseEnter={() => setIsHovered(true)}
-                                   onMouseLeave={() => setIsHovered(false)}
-                                   onClick = {handleToggle}
-                                   >
-                                        <div className="dropdown-sub">
-                                             <span>MORE</span>
-                                             {
-                                                  isToggled ?
-                                                  (
-                                                       isHovered ? 
-                                                       <img src="/images/up-hover.png" className="nav-arrow" /> 
-                                                       : 
-                                                       <img src="/images/up.png" className="nav-arrow" />
-                                                  )
-                                                  :
-                                                  (
-                                                       isHovered ? 
-                                                       <img src="/images/down-hover.png" className="nav-arrow" /> 
-                                                       : 
-                                                       <img src="/images/down.png" className="nav-arrow" />
-                                                  )
-                                             }
-                                             {
-                                                  isToggled && 
-                                                  <div className="dropdown-list-container">
-                                                       <div className="dropdown-list">
-                                                            <Link>Discussions</Link>
-                                                            <Link>Requests</Link>
-                                                            <Link>Contact</Link>
-                                                       </div>
-                                                  </div> 
-                                             }     
-                                        </div>
-                                        {isToggled ? <hr className="hr-1"/> : <hr className="hr-2"/> }
+                              <Link className="link-dropdown">MORE
+                                   <div className="dropdown-sub">
+                                        <div className="dropdown-list-container">
+                                             <div className="dropdown-list">
+                                                  <Link className="sub-links" to="#top">Discussions</Link>
+                                                  <Link className="sub-links">Requests</Link>
+                                                  <Link className="sub-links">Contact</Link>
+                                             </div>
+                                        </div>    
                                    </div>
                               </Link>
                          </div>
