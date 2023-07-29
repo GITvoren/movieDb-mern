52f1806f70cf8705110576ddc8634c8d25faa63d
GITvoren <tejuco.voren@gmail.com>
2023-07-29 09:25:31 +0800

    completed hero slider

--- frontend/src/components/Slider.js
+++ frontend/src/components/Slider.js
@@ -1,58 +1,103 @@
 import slider from '../assets/partial-css/slider.css';
+import { useState, useRef, useEffect } from 'react';
+import slider2 from '../data/slider2.json';
 
 
 function Slider(){
+     const { slides } = slider2;
+     const sliderRef = useRef();
+     const [toggleActiveDot, setToggleActiveDot] = useState(1);
+
+     const slideLeft = () => {
+          if(sliderRef.current.scrollLeft == 0){
+               sliderRef.current.scrollLeft += sliderRef.current.clientWidth * 2;
+               setToggleActiveDot(3)
+          } else {
+               sliderRef.current.scrollLeft -= sliderRef.current.clientWidth;
+
+               if(sliderRef.current.scrollLeft == ( sliderRef.current.scrollWidth - sliderRef.current.clientWidth)) {
+                    setToggleActiveDot(2)
+               } else {
+                    setToggleActiveDot(1)
+               }
+          }
+     }
+
+     const slideRight = () => {
+          
+          if(sliderRef.current.scrollLeft == ( sliderRef.current.scrollWidth - sliderRef.current.clientWidth)){
+               sliderRef.current.scrollLeft -= sliderRef.current.clientWidth * 2
+               setToggleActiveDot(1)
+          } else {
+               sliderRef.current.scrollLeft += sliderRef.current.clientWidth;
+
+               if(sliderRef.current.scrollLeft == 0){
+                    setToggleActiveDot(2)
+               } else {
+                    setToggleActiveDot(3)
+               }
+          }
+          
+     }
+
+     const handleToggleDot = (dotNumber) => {
+          setToggleActiveDot(dotNumber);
+          if(dotNumber == 1){
+               sliderRef.current.scrollLeft = 0
+          } else if(dotNumber == 2){
+               sliderRef.current.scrollLeft = (sliderRef.current.scrollWidth - sliderRef.current.clientWidth) / 2
+          } else {
+               sliderRef.current.scrollLeft = (sliderRef.current.scrollWidth - sliderRef.current.clientWidth)
+          }
+     }
+
+
+
      return(
           <div className="slider-container">
-               <div className="slider">
+               <div className="slider" ref = { sliderRef }>
                      <button 
                     className="slider-btn-2 left d-block"
+                    onClick = { slideLeft }
                     >
                     <img src="/images/slider-left.png" className="arrow-icon left" />
                     </button>
                     <button
                     className="slider-btn-2 right d-block"
+                    onClick = { slideRight }
                     >
                         <img src="/images/slider-right.png" className="arrow-icon right" />
                     </button>
                     <div className="dot-container">
-                              <span className="dot"></span>
-                              <span className="dot active"></span>
-                              <span className="dot"></span>
+                              <span 
+                              className={toggleActiveDot === 1 ? "dot active" : "dot"}
+                              onClick = { () => handleToggleDot(1) }
+                              ></span>
+                              <span 
+                              className={toggleActiveDot === 2 ? "dot active" : "dot"}
+                              onClick = { () => handleToggleDot(2) }
+                              ></span>
+                              <span 
+                              className={toggleActiveDot === 3 ? "dot active" : "dot"}
+                              onClick = { () => handleToggleDot(3) }
+                              ></span>
                     </div>
-                    <div className="slider-img-container">
-                         <img
-                         src="/images/darksouls.jpg"
-                         className="slider-img"
-                         draggable = "false"
-                         />
-                         <div className="slider-content">
-                              <h1 className="slider-content__title">Dark Souls In The Works For Netflix</h1>
-                              <p className="slider-content__desc">Netflix isn't giving up on game adaptations just yet. This would be one of the company's first truly original animation series.</p>
+                    {
+                    slides.map((slide, index) => (
+                         <div className="slider-img-container" key = {index}>
+                              <img
+                              src={slide.src}
+                              className="slider-img"
+                              draggable = "false"
+                              />
+                              <div className="slider-content">
+                                   <h1 className="slider-content__title">{slide.title}</h1>
+                                   <p className="slider-content__desc">{slide.description}</p>
+                              </div>
                          </div>
-                    </div>
-                    <div className="slider-img-container">
-                         <img
-                         src="/images/onepiece.jpg"
-                         className="slider-img"
-                         draggable = "false"
-                         />
-                          <div className="slider-content">
-                              <h1 className="slider-content__title">Lorem</h1>
-                              <p className="slider-content__desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos facilis ipsum distinctio ipsam? Modi, libero?</p>
-                         </div>
-                    </div>
-                    <div className="slider-img-container">
-                         <img
-                         src="/images/devils_plan.jpg"
-                         className="slider-img"
-                         draggable = "false"
-                         />
-                          <div className="slider-content">
-                              <h1 className="slider-content__title">Lorem</h1>
-                              <p className="slider-content__desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos facilis ipsum distinctio ipsam? Modi, libero?</p>
-                         </div>
-                    </div>
+                    ))  
+
+                    }
                </div>
           </div>
      );
